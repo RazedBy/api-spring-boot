9181907b238e3e79a5d26bbd4a0c2755b33a3a83
Léo LETENEUR <leo@Leos-MBP.lan>
2025-04-02 15:15:59 +0200

    Add checkin api arena

--- src/main/java/com/example/tp1/Tp1Application.java
+++ src/main/java/com/example/tp1/Tp1Application.java
@@ -2,12 +2,59 @@ package com.example.tp1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class Tp1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		SpringApplication.run(Tp1Application.class, args);
+    try {
+      Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+      while (interfaces.hasMoreElements()) {
+        NetworkInterface networkInterface = interfaces.nextElement();
+        Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+        while (addresses.hasMoreElements()) {
+          InetAddress inetAddress = addresses.nextElement();
+
+          // Vérifier si c'est une adresse IPv4 et non une loopback
+          if (!inetAddress.isLoopbackAddress() && inetAddress.getHostAddress().indexOf(':') == -1) {
+            String serverIp = inetAddress.getHostAddress();
+            System.out.println("Adresse IP du serveur : " + serverIp);
+
+            RestTemplate restTemplate = new RestTemplate();
+            String apiUrl = "http://51.210.251.137/arena/register";
+
+            // Construire le JSON
+            Map<String, String> requestBody = new HashMap<>();
+            requestBody.put("studentName", "Léo LETENEUR");
+            requestBody.put("baseUrl", "http://" + serverIp + ":8080");
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
+
+            // Envoyer la requête POST
+            ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, request, String.class);
+
+            System.out.println("Réponse de l'API : " + response.getBody());          }
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
 	}
 
 }
